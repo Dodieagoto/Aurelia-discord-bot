aecc2f99e7db8916df8bc30498c352ee4c9c8aff
Dodieagoto <daniilparamonov46@gmail.com>
2026-08-10 14:13:19 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("jvm") version "2.3.21"
+    id("com.gradleup.shadow") version "9.2.2"
+}
+
+group = "dev.dodia"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    // JDA
+    implementation("net.dv8tion:JDA:6.5.0") {
+        exclude(module = "opus-java")
+        exclude(module = "tink")
+    }
+    implementation("club.minnced:jda-ktx:0.15.0")
+
+    //.env
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.2")
+
+    // DB
+    implementation("org.jetbrains.exposed:exposed-core:1.4.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:1.4.0")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:1.4.0")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.5.6")
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+
+        manifest {
+            attributes(
+                "Main-Class" to "ru.ynovka.Main"
+            )
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
---
+++ src/main/kotlin/dev/dodia/Bot.kt
@@ -0,0 +1,34 @@
+package dev.dodia
+
+import dev.minn.jda.ktx.jdabuilder.light
+import io.github.cdimascio.dotenv.Dotenv
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.ChunkingFilter
+import net.dv8tion.jda.api.utils.MemberCachePolicy
+
+class Bot {
+    companion object {
+        lateinit var jda: JDA
+            private set
+
+        private val TOKEN = Dotenv.load().get("TOKEN").toString()
+
+        @JvmStatic
+        fun main(args : Array<String>) {
+
+
+            jda = light(TOKEN){
+                enableIntents(
+                    GatewayIntent.GUILD_MESSAGES,
+                    GatewayIntent.GUILD_MEMBERS,
+                    GatewayIntent.MESSAGE_CONTENT,
+                )
+
+                setMemberCachePolicy(MemberCachePolicy.ALL)
+                setChunkingFilter(ChunkingFilter.ALL)
+            }
+
+        }
+    }
+}
