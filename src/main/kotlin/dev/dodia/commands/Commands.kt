c83613e00f56597006db9ad0baa30b31a19540e3
Dodieagoto <daniilparamonov46@gmail.com>
2026-08-11 03:49:21 +0300

    some stuff

--- src/main/kotlin/dev/dodia/commands/Commands.kt
+++ src/main/kotlin/dev/dodia/commands/Commands.kt
@@ -2,10 +2,8 @@ package dev.dodia.commands
 
 import dev.dodia.Bot.Companion.jda
 import dev.dodia.messages.ProfilePanel
-import dev.minn.jda.ktx.events.listener
 import dev.minn.jda.ktx.events.onCommand
 import dev.minn.jda.ktx.interactions.commands.*
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 
 object Commands {
 
